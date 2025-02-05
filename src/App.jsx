ce2f55458ed710f4a5e2578dedd7308b6e679794
Mohd <mohdfaisal2140@gmail.com>
2025-02-05 22:40:03 +0530

    sign in page integration done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/sign-up/SignUp";
+import SignIn from "./pages/sign-in/SignIn";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </Router>
