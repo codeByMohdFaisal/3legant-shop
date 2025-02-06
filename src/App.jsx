f988d7437ae707747a40c0d175695cc1a83b42f5
Mohd <mohdfaisal2140@gmail.com>
2025-02-07 00:27:40 +0530

    Home page commonheader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import SignUp from "./pages/sign-up/SignUp";
 import SignIn from "./pages/sign-in/SignIn";
+import HomePage1 from "./pages/home-page-1/HomePage1";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to={"/sign-in"} replace />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/home-page-1" element={<HomePage1 />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/home-page-1/HomePage1.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HomePageHeader from "../../components/home-pages/header/HomePageHeader";
+
+const HomePage1 = () => {
+  return (
+    <div>
+      <HomePageHeader
+        bgColor={"#F3F5F7"}
+        textColor={"#343839"}
+        ntfText={"30% off storewide — Limited time!"}
+      />
+    </div>
+  );
+};
+
+export default HomePage1;
